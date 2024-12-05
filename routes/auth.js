f8c533ffb4cad031fed159de8d8337f9b90dcf4d
Apirak Fakin <apirakfakin@gmail.com>
2024-12-05 23:32:02 +0700

    feat: add auth route

---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+require("dotenv").config();
+
+const router = express.Router();
+const SECRET_KEY = process.env.JWT_SECRET || "your-secret-key";
+
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userExists = await User.findOne({ username });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = new User({ username, password });
+    await user.save();
+    
+  } catch {}
+});
